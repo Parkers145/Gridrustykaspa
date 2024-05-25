6faf2d0e4c3b0d1e0578338313124b23989dac04
parkers145 <drewpsmith5@gmail.com>
2024-05-25 12:39:55 -0500

    api testing

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -59,16 +59,14 @@ async fn status(data: web::Data<AppState>) -> impl Responder {
     }
 }
 
-async fn get_logs() -> impl Responder {
-    let file = File::open("/var/log/kaspad.log").map_err(|e| {
-        HttpResponse::InternalServerError().body(format!("Failed to open log file: {}", e))
-    })?;
+async fn get_logs() -> Result<HttpResponse, actix_web::Error> {
+    let file = File::open("/var/log/kaspad.log")
+        .map_err(|e| actix_web::error::ErrorInternalServerError(format!("Failed to open log file: {}", e)))?;
 
     let mut reader = BufReader::new(file);
     let mut contents = String::new();
-    reader.read_to_string(&mut contents).map_err(|e| {
-        HttpResponse::InternalServerError().body(format!("Failed to read log file: {}", e))
-    })?;
+    reader.read_to_string(&mut contents)
+        .map_err(|e| actix_web::error::ErrorInternalServerError(format!("Failed to read log file: {}", e)))?;
 
-    HttpResponse::Ok().body(contents)
+    Ok(HttpResponse::Ok().body(contents))
 }
